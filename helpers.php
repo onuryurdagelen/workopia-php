d93d06738dae52203a1f64b618c4eb90f30c6df8
Onur Yurdagelen <yurdagelenonur7@gmail.com>
2024-10-16 18:48:54 +0300

    Initial commit and started home view

---
+++ helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Get the base path
+ * 
+ * @param string path
+ * @return string
+ */
+function basePath($path = ''): string
+{
+
+    return __DIR__ . '/' . $path;
+}
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+require '../helpers.php';
+//php -S localhost -t public
+require basePath('views/home.view.php');
